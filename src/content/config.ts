09d04a4a46f835a9e40e8dd759b1694b8099a418
Vidish Raj <66262548+vidishraj@users.noreply.github.com>
2025-06-07 14:32:14 +0530

    refacto: Remove validation for flexible .md files.

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,12 +2,12 @@ import { defineCollection, z } from 'astro:content';
 
 const docsCollection = defineCollection({
   type: 'content',
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    pubDate: z.date(),
-    images: z.array(z.string())
-  })
+  // schema: z.object({
+  //   title: z.string(),
+  //   description: z.string(),
+  //   pubDate: z.date(),
+  //   images: z.array(z.string())
+  // })
 });
 
 export const collections = {
